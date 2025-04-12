7dc7b921d40c7ad70d0a0746da7a554345de3dfb
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-04-12 19:10:55 +0530

    Add files via upload
    performed operations on bit:
    1. SetBit on specified position
    2. GetBit on specified position
    3. ClearBit from specified position
    4. UpdateBit on specified position

---
+++ BitManipulation.java
@@ -0,0 +1,62 @@
+package alpha;
+
+import java.util.Scanner;
+
+public class BitManipulation {
+	public static void oddOrEven(int n) {
+		int bitmask = 1;
+		if ((n & bitmask) == 0)
+			System.out.println("Even Number ");
+		else
+			System.out.println("Odd number");
+	}
+
+	public static byte getIthBit(int n, int i) { // i = position of bit
+		int bitmask = 1 << i;
+		if ((n & bitmask) == 0)
+			return 0;
+		else
+			return 1;
+
+	}
+
+	public static int setIthBit(int n, int i) {
+		int bitmask = 1 << i;
+		return n | bitmask;
+	}
+
+	public static int clearIthBit(int n, int i) {
+		int bitmask = ~(1 << i);
+		return n & bitmask;
+	}
+
+	public static int updateIthBit(int n, int i, byte newBit) {
+//		if(newBit == 0)
+//			return clearIthBit(n,i);
+//		else 
+//			return setIthBit(n,i);
+//		
+// or other way
+
+		n = clearIthBit(n, i);
+		int bitMask = newBit << i;
+		return n | bitMask;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+//		System.out.print("Enter a number to find Odd or Even: ");
+//		oddOrEven(sc.nextInt());
+
+		// System.out.print("Enter a number and position to find bit: ");
+		// System.out.println(getIthBit(sc.nextInt(), sc.nextInt()));
+
+//		System.out.print("Enter a number and position to clear bit: ");
+//		System.out.println(clearIthBit(sc.nextInt(), sc.nextInt()));
+
+		System.out.println(updateIthBit(10, 2, (byte) 1));
+		sc.close();
+	}
+
+}
