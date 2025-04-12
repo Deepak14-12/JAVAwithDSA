f8297378c90a330b5d69f243e95cb8f28a0c808f
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-04-12 19:14:06 +0530

    Update BitManipulation.java

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,5 +1,3 @@
-package alpha;
-
 import java.util.Scanner;
 
 public class BitManipulation {
@@ -31,12 +29,12 @@ public class BitManipulation {
 	}
 
 	public static int updateIthBit(int n, int i, byte newBit) {
-//		if(newBit == 0)
-//			return clearIthBit(n,i);
-//		else 
-//			return setIthBit(n,i);
-//		
-// or other way
+		if(newBit == 0)
+			return clearIthBit(n,i);
+		else 
+			return setIthBit(n,i);
+		
+or other way
 
 		n = clearIthBit(n, i);
 		int bitMask = newBit << i;
@@ -45,17 +43,22 @@ public class BitManipulation {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+        //Odd or Even number
+		System.out.print("Enter a number to find Odd or Even: ");
+		oddOrEven(sc.nextInt());
+//Set BIt
+		System.out.print("Enter a number and position to Set bit: ");
+		System.out.println(setIthBit(sc.nextInt(), sc.nextInt()));
 
-//		System.out.print("Enter a number to find Odd or Even: ");
-//		oddOrEven(sc.nextInt());
-
-		// System.out.print("Enter a number and position to find bit: ");
-		// System.out.println(getIthBit(sc.nextInt(), sc.nextInt()));
-
-//		System.out.print("Enter a number and position to clear bit: ");
-//		System.out.println(clearIthBit(sc.nextInt(), sc.nextInt()));
-
+//get BIt
+		/System.out.print("Enter a number and position to find bit: ");
+		System.out.println(getIthBit(sc.nextInt(), sc.nextInt()));
+//clear bit
+		System.out.print("Enter a number and position to clear bit: ");
+		System.out.println(clearIthBit(sc.nextInt(), sc.nextInt()));
+//updatebit
 		System.out.println(updateIthBit(10, 2, (byte) 1));
+		
 		sc.close();
 	}
 
