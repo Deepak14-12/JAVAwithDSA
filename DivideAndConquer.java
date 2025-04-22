b01592d03c17bafc5fef030a6be137157a647b22
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-04-22 23:58:12 +0530

    Create DivideAndConquer.java

---
+++ DivideAndConquer.java
@@ -0,0 +1,51 @@
+public class DivideAndConquer {
+
+	public void printArray(int arr[]) {
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+	}
+
+
+
+	public void merge(int arr[], int startIndex, int mid, int endIndex) {
+		int temp[] = new int[endIndex - startIndex + 1];
+		int i = startIndex; // iterator for left part
+		int j = mid + 1; // iterator for right part
+		int k = 0; // iterator for temporary array
+
+		while (i <= mid && j <= endIndex) {
+			if (arr[i] < arr[j]) {
+				temp[k++] = arr[i++];
+			} else {
+				temp[k++] = arr[j++];
+			}
+		}
+
+//		left part
+		while (i <= mid) {
+			temp[k++] = arr[i++];
+		}
+//		right part
+		while (j <= endIndex) {
+			temp[k++] = arr[j++];
+		}
+		// copy temp to orignal array
+		for (k = 0, i = startIndex; k < temp.length; k++, i++) {
+			arr[i] = temp[k];
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 6, 1, 4, 2, 9, 8 };
+    
+		DivideAndConquer ms = new DivideAndConquer();
+		
+    System.out.println("\nbefore merge sort: ");
+		ms.printArray(arr);
+		ms.mergeSort(arr, 0, 5); //merge sort
+		System.out.println("\nafter merge sort: ");
+		ms.printArray(arr);
+	}
+
+}
