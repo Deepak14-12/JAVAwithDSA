64492dfe0d40fdab3d458d3e88bc25d3412bbfd7
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-04-25 11:53:30 +0530

    Update HeapSort.java (added comments to understand working)

--- HeapSort.java
+++ HeapSort.java
@@ -1,9 +1,7 @@
-package alpha;
-
 import java.util.Scanner;
 
 public class HeapSort {
-
+               //sorting methos for heap sort
 	void heapSort(int arr[], int n) {
 
 		// build heap
@@ -17,7 +15,7 @@ public class HeapSort {
 			heapify(arr, i, 0);
 		}
 	}
-
+           //helper method for heap sort 
 	void heapify(int arr[], int n, int i) {
 		int largest = i;
 		int left = 2 * i + 1;
@@ -35,19 +33,19 @@ public class HeapSort {
 
 		}
 	}
-
+              //sawap elements
 	void swap(int arr[], int a, int b) {
 		int temp = arr[a];
 		arr[a] = arr[b];
 		arr[b] = temp;
 	}
-
+               //print array
 	static public void printArray(int arr[]) {
 		for (int i : arr) {
 			System.out.print(i + " ");
 		}
 	}
-
+          //scane array elements from user
 	static public void getElementsInArray(int arr[]) {
 		System.out.println("Enter element in array to sort them: ");
 		for (int i = 0; i < arr.length; i++) {
@@ -59,14 +57,20 @@ public class HeapSort {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter number of element you want to insert: ");
 		int arr[] = new int[sc.nextInt()];
-
+		
+                //taking array elements from user
+		getElementsInArray(arr);
+		//object creation
 		HeapSort hs = new HeapSort();
+		
 		// sort using heap sort
 		hs.heapSort(arr, arr.length);
-
+                
+		//printing sorted elements
 		System.out.println("sorted array : ");
 		printArray(arr);
 
+		//closing the Scanner class object sc
 		sc.close();
 	}
 
