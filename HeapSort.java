ad53bb844c67b7a6914078df4bcf59e3c78ba1b2
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-04-25 11:46:19 +0530

    Create HeapSort.java

---
+++ HeapSort.java
@@ -0,0 +1,71 @@
+package alpha;
+
+import java.util.Scanner;
+
+public class HeapSort {
+
+	void heapSort(int arr[], int n) {
+
+		// build heap
+		for (int i = n / 2 - 1; i >= 0; i--) {
+			heapify(arr, n, i);
+		}
+
+		// deletion one by one
+		for (int i = n - 1; i > 0; i--) {
+			swap(arr, 0, i);
+			heapify(arr, i, 0);
+		}
+	}
+
+	void heapify(int arr[], int n, int i) {
+		int largest = i;
+		int left = 2 * i + 1;
+		int right = 2 * i + 2;
+
+		if (left < n && arr[left] > arr[largest])
+			largest = left;
+
+		if (right < n && arr[right] > arr[largest])
+			largest = right;
+
+		if (largest != i) {
+			swap(arr, i, largest);
+			heapify(arr, n, largest);
+
+		}
+	}
+
+	void swap(int arr[], int a, int b) {
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+
+	static public void printArray(int arr[]) {
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of element you want to insert: ");
+		int arr[] = new int[sc.nextInt()];
+
+		System.out.println("Enter element in array to sort them: ");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		HeapSort hs = new HeapSort();
+		// sort using heap sort
+		hs.heapSort(arr, arr.length);
+
+		System.out.println("sorted array : ");
+		printArray(arr);
+
+		sc.close();
+	}
+
+}
