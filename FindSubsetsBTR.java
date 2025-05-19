1473a909137b2c99cb361b57e0a8938ca12f3e21
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-05-20 01:19:28 +0530

    Create FindSubsetsBTR.java

---
+++ FindSubsetsBTR.java
@@ -0,0 +1,27 @@
+//problem: find & print all subsets of a given string.
+public class FindSubsetsBTR {
+	public static void findSubsets(String str, String ans, int index) {
+		// base case
+		if (index == str.length()) {
+			if (ans.length() == 0)
+				System.out.println("null");
+			else
+				System.out.println(ans);
+			return;
+		}
+		
+		//yes choice
+		findSubsets(str, ans+str.charAt(index), index+1);
+		//no choice
+		findSubsets(str, ans, index+1);
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String  str = "abcd";
+		findSubsets(str," ",0);
+
+	}
+
+}
