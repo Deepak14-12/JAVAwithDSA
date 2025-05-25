c8fe00f517b79212a9791d816bb98cca0ab01b98
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-05-25 15:57:54 +0530

    added description for example 8

--- AutoboxingUnboxing.java
+++ AutoboxingUnboxing.java
@@ -42,28 +42,39 @@ public class AutoboxingUnboxing
     System.out.println(x==y); //false
       }
   -----------------------------------
-  
+  //example 4:
     Integer x = new Integer(10) ; 
     Integer y = new Integer(10) ; 
     System.out.println(x==y); //false
       
   -----------------------------------
+    //example 5:
     Integer x = new Integer(10) ; 
     Integer y = 10 ; 
     System.out.println(x==y); //false
       
   -----------------------------------
+    //example 6:
     Integer x = 10; 
     Integer y = 10 ; 
     System.out.println(x==y); //true
       
   -----------------------------------
+   //example 7:
     Integer x = 100; 
     Integer y = 100; 
     System.out.println(x==y); //true
   -----------------------------------
+    //example 8:
     Integer x = 1000; 
     Integer y = 1000; 
     System.out.println(x==y); //false
  */   
+  /** in example 8 o/p is false because :
+    * to implement autoboxing concept in every wrapper class a buffer of object will be created at the time of class loading.
+    * by autoboxing of an object is required to create then JVM will check for the avalibily of that object in buffer.
+    * if object not available in buffer then new object will be created.
+    * the only range availble in buffer is from -128 to 127 for every wrapper class.
+    * thats why it gives false in example 8.  
+  */
 }
