bf072282527ac8e5adee315212950d25cffa349c
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-05-25 15:28:59 +0530

    Update AutoboxingUnboxing.java (added examples)

--- AutoboxingUnboxing.java
+++ AutoboxingUnboxing.java
@@ -13,5 +13,22 @@ public class AutoboxingUnboxing
     int m = K; //---4---AUB
     System.out.println(m);  //10
   }
-
+/*------------------------------------
+ * example 1:
+  static Integer I = 0;
+   public static void main(String[] args)
+  {
+    int i = I; 
+    System.out.println(i); // 0
+      }
+  ------------------------------------
+  * example 2:
+  static Integer I ;  //null 
+   public static void main(String[] args)
+  {
+    int i = I; 
+    System.out.println(i); // RE: nullPointerException
+      }
+  -----------------------------------
+ */   
 }
