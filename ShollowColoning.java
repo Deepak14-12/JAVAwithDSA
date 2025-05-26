014f1ade3cd3a1a71dd1ea1612a17eefe8b3ba05
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-05-26 10:08:17 +0530

    Create ShollowColoning.java

---
+++ ShollowColoning.java
@@ -0,0 +1,36 @@
+package langpack;
+//Shallow cloning of objects
+class Cat 
+{
+  int j ;
+  Cat(int j)
+  { 
+   this.j = j;
+  }  
+}
+class Dog implements Cloneable
+{
+  Cat c;
+  int i ;
+  Dog(Cat c, int i)
+  { 
+   this.c = c;
+   this.i = i;
+  }  
+ public Object clone() throws CloneNotSupportedException
+ {
+   return super.clone();
+ }
+}
+class ShollowColoning
+{
+ public static void main(String[] args) throws   CloneNotSupportedException
+ {
+   Cat c = new Cat(10);
+   Dog d1 = new Dog(c,10);
+   Dog d2= (Dog) d1.clone();
+   d2.i = 888;
+   d2.c.j = 999;
+   System.out.println(d1.i+"-----"+d1.c.j);
+ }
+}
