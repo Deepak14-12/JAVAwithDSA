77f10adfa4632fdcf2d4af25f95441b42e4a7058
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-05-26 19:51:31 +0530

    Create FileReaderTests.java

---
+++ FileReaderTests.java
@@ -0,0 +1,27 @@
+package filehandling;
+import java.io.*;
+
+//FileWriter: using FileWriter obj we can write character data to the file 
+
+class FileReaderTests
+{
+ public static void main(String [] args) throws IOException
+  {  
+//------------------------read()-----------------------------------
+ /*FileReader fr = new FileReader("D:\\JAVA\\collegedemo\\src\\house1\\khairi.txt");
+       
+   int i = fr.read(); 
+   while(i != -1)
+   {
+    System.out.print((char)i);
+    i = fr.read();
+   }*/
+//-----------------read(char[] ch)-----------------------------------
+   File f = new File("D:\\JAVA\\collegedemo\\src\\house1\\khairi.txt");
+   char []ch = new char[(int)f.length()]; 
+   FileReader fr1 = new FileReader(f);
+   fr1.read(ch); 
+   for(char ch1:ch)
+      System.out.print(ch);     
+  }
+}
