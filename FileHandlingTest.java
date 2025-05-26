fea7185f13b5c420804307c87b5ebcf3a7f7f109
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-05-26 17:56:17 +0530

    Update FileHandlingTest.java

--- FileHandlingTest.java
+++ FileHandlingTest.java
@@ -5,23 +5,23 @@ class FileHandlingTest
  public static void main(String [] args) throws IOException
  {/*
    File f = new File("house.txt"); //represents a file or dir only not creates
-   System.out.println(f.exists()); 
+   System.out.println(f.exists()); // false
    f.createNewFile(); // creates file in current working directory
-   System.out.println(f.exists()); 
+   System.out.println(f.exists()); //true
 //-------------------------------------
    File f = new File("house");
-   System.out.println(f.exists()); 
+   System.out.println(f.exists()); //false
    f.mkdir(); // creates directory in current working directory 
-   System.out.println(f.exists()); 
+   System.out.println(f.exists()); //true
 //-------------------------------------------
   File f = new File("D:\\JAVA\\collegedemo\\src\\house","abc.txt");
-  System.out.println(f.exists()); 
+  System.out.println(f.exists()); //true
   f.createNewFile();  // creates file in specified subdirectopry that are in current working directory
-  System.out.println(f.exists()); 
+  System.out.println(f.exists()); //true
 */
 //---------------------------------------
   File f = new File("house1","khairi.txt");
-  System.out.println(f.exists()); 
+  System.out.println(f.exists()); false
   f.createNewFile();//RE:IOE -System cannot find path bcz house1 is not there
   System.out.println(f.exists()); 
  }
