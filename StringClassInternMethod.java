41918c0a96ef2fa57ea73c7041aa2c296d6875cd
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-05-26 11:10:46 +0530

    Update StringClassInternMethod.java

--- StringClassInternMethod.java
+++ StringClassInternMethod.java
@@ -8,5 +8,13 @@ String s2 = s1.intern();
 System.out.println(s1==s2);//false 
 String s3 = "bhaskar"; 
 System.out.println(s2 == s3);//true
+  
+String s4 = new String("Durga"); 
+String s5 = s4.concat("Software");
+String s6 = s5.intern();
+System.out.println(s5==s6);//false 
+String s7 = "DurgaSoftware"; 
+System.out.println(s6 == s7);//true
+
   }
 }
