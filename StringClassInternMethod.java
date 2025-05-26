1efc50df47a878b8080991647c752d11b87183f1
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-05-26 10:52:45 +0530

    Create StringClassInternMethod.java

---
+++ StringClassInternMethod.java
@@ -0,0 +1,12 @@
+package langpack;
+class StringClassInternMethod
+{
+ public static void main(String[] args)
+ { 
+String s1 = new String("bhaskar"); 
+String s2 = s1.intern();
+System.out.println(s1==s2);//false 
+String s3 = "bhaskar"; 
+System.out.println(s2 == s3);//true
+  }
+}
