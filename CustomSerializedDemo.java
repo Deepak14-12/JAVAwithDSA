be5572958be6a09554b49d2a66ff00d787a4ccdf
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-05-28 15:13:09 +0530

    Update CustomSerializedDemo.java

--- CustomSerializedDemo.java
+++ CustomSerializedDemo.java
@@ -1,4 +1,5 @@
 //demo program for customized serialization to recover loss of information which is happen due to transient keyword.
+
 package serialization;
 import java.io.*;
 
@@ -6,17 +7,22 @@ class Account implements Serializable
 {
  String username = "deepak";
  transient String pwd= "rush";
+ transient int pin= 1234;
  private void writeObject(ObjectOutputStream os) throws Exception
  {
   os.defaultWriteObject();
   String epwd = "123"+pwd;
+  int epin = 4444+pin;
   os.writeObject(epwd);
+  os.writeInt(epin);
 }
  private void readObject(ObjectInputStream is) throws Exception
  {
   is.defaultReadObject();
   String epwd = (String)is.readObject();
   pwd = epwd.substring(3);
+  int epin = is.readInt();
+  pin = epin-4444;
  }
 }
 class CustomSerializedDemo
@@ -26,7 +32,7 @@ class CustomSerializedDemo
    Account a1 = new Account();
 // Serialization
    System.out.println("Serialization started...");
-   System.out.println(a1.username+"  "+a1.pwd);
+   System.out.println(a1.username+"  "+a1.pwd+"  "+a1.pin);
    FileOutputStream fos = new FileOutputStream("Account.ser");
    ObjectOutputStream oos = new ObjectOutputStream(fos);
    oos.writeObject(a1); 
@@ -37,6 +43,6 @@ class CustomSerializedDemo
    ObjectInputStream ois = new ObjectInputStream(fis);
    Account a2 = (Account)ois.readObject();
    System.out.println("De-Serialization ended...");  
-   System.out.println("Username: "+a2.username+"  password: "+a2.pwd);
+   System.out.println("Username: "+a2.username+"  password: "+a2.pwd+"  pin: "+a2.pin);
   }
 }
