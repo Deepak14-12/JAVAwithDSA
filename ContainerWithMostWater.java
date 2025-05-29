da88f0e2e1e0a70e912356efc2a45923074ce8e6
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-05-30 01:11:11 +0530

    Create ContainerWithMostWater.java

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,38 @@
+//leetcode problem: Container With Most Water
+package alpha;
+import java.util.ArrayList;
+
+public class ContainerWithMostWater {
+	// Brute Force Approach TC = O(n^2)
+	public static int saveWaterBF(ArrayList<Integer> heights) {
+		int maxWater = 0;
+		// Calculate Water Area
+		for (int i = 0; i < heights.size(); i++) {
+			for (int j = i + 1; j < heights.size(); j++) {
+				int height = Math.min(heights.get(i), heights.get(j));
+				int width = j - i;
+				int currentWater = height * width;
+				maxWater = Math.max(maxWater, currentWater);
+			}
+		}
+		return maxWater;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> heights = new ArrayList<>();
+		heights.add(1);
+		heights.add(8);
+		heights.add(6);
+		heights.add(2);
+		heights.add(5);
+		heights.add(4);
+		heights.add(8);
+		heights.add(3);
+		heights.add(7);
+
+		 System.out.println("max water can be hold: " + saveWaterBF(heights));
+
+	}
+
+}
