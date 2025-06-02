bbf5bfc4445ffc9c99edfcb49c0eba6d7f614d24
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-06-02 16:16:58 +0530

    Create ListIteratorDemo.java

---
+++ ListIteratorDemo.java
@@ -0,0 +1,30 @@
+package collectionf;
+import java.util.*;
+class ListIteratorDemo
+{
+ public static void main(String[] args)
+ {
+  LinkedList<String> ll = new LinkedList<>();
+  ll.add("deepak");
+  ll.add("tushar");
+  ll.add("trisha");
+  ll.add("harshu");
+  ll.add("yaadu");
+  ll.add("maanu");
+  System.out.println(ll);
+  
+  ListIterator itr = ll.listIterator();
+  while(itr.hasNext())
+  {
+    String s = (String) itr.next();
+
+    if(s.equals("trisha"))
+      itr.remove();
+    else if(s.equals("deepak"))
+      itr.add("manish");
+    else if(s.equals("yaadu"))
+      itr.set("yaadvi"); 
+  }
+  System.out.println(ll);
+ }
+}
