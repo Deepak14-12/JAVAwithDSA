907c435ceadd383942d80d150516673b1ebb5a0d
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-06-04 23:41:34 +0530

    added isCycle method

--- LinkedList.java
+++ LinkedList.java
@@ -233,6 +233,18 @@ public class LinkedList {
 		return true;
 	}
 
+	//Detect a loop/Cycle in a LL
+	public static boolean isCycle() {
+		Node slow = head;
+		Node fast = head;
+		while(fast != null && fast.next  != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if(slow == fast)
+				return true;  //cycle exist
+		}
+		return false;  //cycle not exist
+	}
 	public static void main(String[] args) {
 		LinkedList ll = new LinkedList();
 		ll.printList();
@@ -248,21 +260,35 @@ public class LinkedList {
 		ll.printList();
 		ll.addInMiddle(3, 21);
 		ll.printList();
+		
 		System.out.println("size of linked list: " + size);
 		System.out.println("removed element: " + ll.removeFirst());
 		ll.printList();
+		
 		System.out.println("size of linked list: " + size);
 		System.out.println("removed element: " + ll.removeLast());
 		ll.printList();
+		
 		System.out.println("size of linked list: " + size);
 		System.out.println("(Iterative search)Key found at: " + ll.itrSearch(21));
 
 		System.out.println("(Recursive search)key found at: " + ll.recursiveSearch(11));
 		ll.reverseITR();
 		ll.printList();
+		
 		ll.deleteNthFromEnd(3); // 3rd element from end
 		ll.printList();
-		System.out.println(ll.checkPalindrome()); //check ll is palindrome ?
+		
+		System.out.println("is ll Palindrome? ="+ll.checkPalindrome()); //check ll is palindrome ?
+		
+	/*	head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head.next.next.next = head;
+		*/
+		boolean isCycle = isCycle();
+		System.out.println("Is LL Cyclic? ="+isCycle);
+		
 	}
 
 }
