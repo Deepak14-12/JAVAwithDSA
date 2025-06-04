31ab6c13c6b44c7893b92c6774412fb0b8f4bcfd
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-06-04 14:20:31 +0530

    Create TreeSetDemoS.java

---
+++ TreeSetDemoS.java
@@ -0,0 +1,27 @@
+package collectionf;
+import java.util.*;
+class TreeSetDemoS
+{
+ public static void main(String[] args)
+ {
+   TreeSet t = new TreeSet(new MyComparatorS());
+   t.add("kartikey");
+   t.add("vinesh");
+   t.add("vikesh");
+   t.add("divya");
+   t.add("deepak");
+System.out.println(t);
+ }
+}
+
+class MyComparatorS implements Comparator
+{
+ public int compare(Object obj1 , Object obj2)
+ {
+  String s1 = (String) obj1; //typecasting applicable only for internal strings 
+  String s2 = obj2.toString(); //applicable for all SBr,SBf, and Strings 
+  //returning descending order
+    return -s1.compareTo(s2);
+ // return s2.compareTo(s1); both statements returns same order
+ }
+}
