c69e2e092253b85e34f19fb4241eba9e1ba22061
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-06-07 22:09:26 +0530

    Update DoublyLInkedList.java

--- DoublyLInkedList.java
+++ DoublyLInkedList.java
@@ -1,4 +1,7 @@
 package collectionframework;
+
+import alpha.LinkedList.Node;
+
 public class DoublyLInkedList {
 	public class Node {
 		int data;
@@ -29,7 +32,7 @@ public class DoublyLInkedList {
 		head.prev = newNode;
 		head = newNode;
 	}
-	
+
 	// add node AT Last
 	public void addLast(int data) {
 		// step-1 create new node
@@ -39,11 +42,34 @@ public class DoublyLInkedList {
 			head = tail = newNode;
 			return;
 		}
-		
-		// step-2 
+
+		// step-2
 		newNode.prev = tail;
 		tail.next = newNode;
-        tail = newNode;
+		tail = newNode;
+	}
+
+	// add in middle of list
+	public void addInMiddle(int index, int data) {
+		// step-1 create new node
+		Node newNode = new Node(data);
+		if (index == 0) {
+			addFirst(data);
+			return;
+		}
+		Node temp = head;
+		int i = 0;
+		// step-2 traverse till index-1
+		while (i < index - 1) {
+			temp = temp.next;
+			i++;
+		}
+		// step-3 add links
+		newNode.next = temp.next;
+		newNode.prev = temp;
+		newNode.next.prev = newNode;
+		temp.next = newNode;
+		size++;
 	}
 
 	// remove node AT First
@@ -64,22 +90,43 @@ public class DoublyLInkedList {
 		return value;
 
 	}
+
+	// remove last node
+	public static int removeLast() {
+		if (size == 0) { // if(head==null)
+			System.out.println("linked list is empty.");
+			return Integer.MIN_VALUE;
+		} else if (size == 1) {
+			int value = head.data;
+			head = tail = null;
+			size = 0;
+			return value;
+		}
+		int value = tail.data;
+		tail = tail.prev;
+		tail.next = null;
+		size--;
+		return value;
+
+	}
+
 //reverse DoublyLL
 	public void reverseLL() {
-	Node current = head;
-	Node prev = null;
-	Node next;
-	
-	while(current != null) {
-		next = current.next;
-		current.next = prev;
-		current.prev = next;
-		
-		prev = current;
-		current = next;
+		Node current = head;
+		Node prev = null;
+		Node next;
+
+		while (current != null) {
+			next = current.next;
+			current.next = prev;
+			current.prev = next;
+
+			prev = current;
+			current = next;
+		}
+		head = prev;
 	}
-	head = prev;
-	}
+
 	// print linked list
 	public void printList() {
 		if (head == null) {
@@ -103,13 +150,24 @@ public class DoublyLInkedList {
 		addFirst(13);
 		addFirst(14);
 		dll.printList();
+
 		dll.addLast(56);
 		dll.printList();
-		System.out.println(size);
+		System.out.println("side of list: " + size);
+
 		int removedNode = removeFirst();
-		System.out.println("removed element: "+removedNode);
-		System.out.println(size);
+		System.out.println("removed element: " + removedNode);
+		System.out.println("size of list: " + size);
+
 		dll.reverseLL();
+		dll.printList();
+
+		int rml = removeLast();
+		System.out.println("removed element: " + rml + "\nsize of list: " + size);
+		dll.printList();
+		dll.printList();
+
+		dll.addInMiddle(3, 5);
 		dll.printList();
 	}
 }
