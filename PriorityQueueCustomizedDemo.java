c76c2eade4ce46ae7ce0f73458d24a8b37175192
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-06-08 12:45:56 +0530

    Create PriorityQueueCustomizedDemo.java

---
+++ PriorityQueueCustomizedDemo.java
@@ -0,0 +1,23 @@
+class PriorityQueueCustomizedDemo
+{
+ public static void main(String [] args)
+ {
+  PriorityQueue q = new PriorityQueue(15, new MyComparatorQ());
+  q.offer("A");
+  q.offer("Z");
+  q.offer("L");  
+  q.offer("B");
+  System.out.println(q); //[Z,B,L,A]
+    
+ }
+}
+
+class MyComparatorQ implements Comparator
+{
+  public int compare(Object obj1, Object obj2)
+  {
+   String s1 = (String) obj1;
+   String s2 = obj2.toString();
+   return s2.compareTo(s1);
+  }
+} explain the output
