07fd0bc4e56f939a7beb46f8e9aa31595d8c193d
Deepak Bisen <136325518+Deepak14-12@users.noreply.github.com>
2025-06-20 15:13:31 +0530

    Update EnumConstructor.java

--- EnumConstructor.java
+++ EnumConstructor.java
@@ -1,9 +1,27 @@
 package enumm;
 
 enum Beer1{
- KF,KO,RC,FO; //HERE SEMICOLON IS OPTIONAL
+ KF,KO,RC,FO;
 Beer1(){
 System.out.println("Costructor called.");
+}
+}
+
+enum BeerB{
+ KF(70),KO(100),RC(90),FO;
+int price;
+
+BeerB(){
+this.price = 65;
+}
+
+BeerB(int price){
+this.price = price;
+}
+
+public int getPrice()
+{
+ return price;
 }
 }
 
@@ -14,5 +32,9 @@ public static void main(String[] args){
 Beer1 b = Beer1.RC;
   System.out.println("hello...");
 
+
+BeerB[] b1 = BeerB.values();
+for(BeerB bb: b1)
+  System.out.println(bb+"..."+bb.getPrice());
 }
 }
